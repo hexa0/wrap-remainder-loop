41ae3d16c24f1afb27c850d31559b89869bf6838
hexa0 <67880600+hexa0@users.noreply.github.com>
2024-12-26 00:59:07 -0500

    edit sampler chunk

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,46 +1,70 @@
 import assert from "assert";
-import { GetSampleCount, ReadSample, ReadWaveHeaders, WriteSample } from "../spec/wave";
+import {
+	GetSampleCount,
+	ReadSample,
+	ReadWaveHeaders,
+	WriteSample,
+} from "../spec/wave";
 import { ReadWaveCueChunk } from "../spec/wave/waveCueChunk";
-import { ReadWaveSamplerChunk } from "../spec/wave/waveSamplerChunk";
+import {
+	ReadWaveSamplerChunk,
+	WriteWaveSamplerChunk,
+} from "../spec/wave/waveSamplerChunk";
 import { RiffInterfaceToBuffer } from "../spec/riff";
 import { writeFileSync } from "fs";
 
 export function RenderNewWave(file: Buffer, outputPath: string) {
 	const waveFile = ReadWaveHeaders(file);
 
-	const smplChunk = waveFile.riff.chunks.get("smpl");
-	assert(smplChunk, `No smpl chunk found, Make sure you're using the correct export settings in the readme`);
-	
-	const cueChunk = waveFile.riff.chunks.get("cue");
-	assert(cueChunk, `No cue chunk found, Make sure you're using the correct export settings in the readme`);
+	const smpl = waveFile.riff.chunks.get("smpl");
+	assert(
+		smpl,
+		`No smpl chunk found, Make sure you're using the correct export settings in the readme`
+	);
 
-	const loopMarker = ReadWaveCueChunk(cueChunk).points[0]
-	assert(loopMarker, `No loopMarker found, Make sure you're using the correct export settings in the readme`)
+	const cue = waveFile.riff.chunks.get("cue");
+	assert(
+		cue,
+		`No cue chunk found, Make sure you're using the correct export settings in the readme`
+	);
 
-	const loopRegion = ReadWaveSamplerChunk(smplChunk).loops[0]
-	assert(loopRegion, `No loopRegion found, Make sure you're using the correct export settings in the readme`)
+	const cueData = ReadWaveCueChunk(cue);
+	assert(
+		cueData.points[0],
+		`No loopMarker found, Make sure you're using the correct export settings in the readme`
+	);
 
-	const remainderStart = loopRegion.end
-	const remainderSize = GetSampleCount(waveFile) - remainderStart
-	const loopStart = loopMarker.position
+	const samplerData = ReadWaveSamplerChunk(smpl);
+	assert(
+		samplerData.loops[0],
+		`No loopRegion found, Make sure you're using the correct export settings in the readme`
+	);
+
+	const sampleCount = GetSampleCount(waveFile);
+	const remainderStart = samplerData.loops[0].end;
+	const remainderSize = sampleCount - remainderStart;
+	const loopStart = cueData.points[0].position;
 
 	for (let index = 0; index < remainderSize; index++) {
-		const readRemainder = ReadSample(waveFile, remainderStart + index)
-		const readLoopWrap = ReadSample(waveFile, loopStart + index)
+		const readRemainder = ReadSample(waveFile, remainderStart + index);
+		const readLoopWrap = ReadSample(waveFile, loopStart + index);
 
 		const write = [];
 		for (let index = 0; index < waveFile.fmt.channels; index++) {
-			write[index] = readRemainder[index] + readLoopWrap[index]
+			write[index] = readRemainder[index] + readLoopWrap[index];
 		}
 
-		WriteSample(waveFile, remainderStart + index, write)
+		WriteSample(waveFile, remainderStart + index, write);
 	}
 
-	waveFile.riff.chunks.set("data", waveFile.data)
+	waveFile.riff.chunks.set("data", waveFile.data);
 
-	// console.log(remainderStart, remainderSize, loopStart)
+	samplerData.loops[0].start = loopStart + remainderSize;
+	samplerData.loops[0].end = sampleCount;
 
-	const newFileBuffer = RiffInterfaceToBuffer(waveFile.riff)
+	samplerData.buffer = WriteWaveSamplerChunk(samplerData);
 
-	writeFileSync(outputPath, newFileBuffer)
+	const newFileBuffer = RiffInterfaceToBuffer(waveFile.riff);
+
+	writeFileSync(outputPath, newFileBuffer);
 }
--- src/spec/wave/waveSamplerChunk.ts
+++ src/spec/wave/waveSamplerChunk.ts
@@ -58,3 +58,34 @@ export function ReadWaveSamplerChunk(chunk: RiffChunk): WaveSamplerChunk {
 
 	return samplerChunk;
 }
+
+export function WriteWaveSamplerChunk(chunk: WaveSamplerChunk): Buffer {
+	const buffer = Buffer.alloc(36 + chunk.loops.length * 24);
+	const view = new DataView(chunk.buffer.buffer);
+
+	chunk.sampleLoops = chunk.loops.length;
+
+	view.setUint32(0, chunk.manufacturer, true);
+	view.setUint32(4, chunk.product, true);
+	view.setUint32(8, chunk.samplePeriod, true);
+	view.setUint32(12, chunk.midiUnityNote, true);
+	view.setUint32(16, chunk.midiPitchFraction, true);
+	view.setUint32(20, chunk.smpteFormat, true);
+	view.setUint32(24, chunk.smpteOffset, true);
+	view.setUint32(28, chunk.sampleLoops, true);
+	view.setUint32(32, chunk.samplerData, true);
+
+	for (let index = 0; index < chunk.sampleLoops; index++) {
+		const offset = 36 + index * 24;
+		const loop = chunk.loops[index];
+
+		view.setUint32(offset + 0, loop.identifier, true);
+		view.setUint32(offset + 4, loop.type, true);
+		view.setUint32(offset + 8, loop.start, true);
+		view.setUint32(offset + 12, loop.end, true);
+		view.setUint32(offset + 16, loop.fraction, true);
+		view.setUint32(offset + 20, loop.playCount, true);
+	}
+
+	return buffer
+}
